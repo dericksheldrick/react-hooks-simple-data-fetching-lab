029d704fe0870c6e7700ec68f66f0440a56eb3f0
Derick Sheldrick <sheldrickderick9@gmail.com>
2025-04-22 18:17:44 +0300

    fetching data submission

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,21 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [dogPic, setDogPic] = useState(null);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setDogPic(data.message);
+      });
+  }, []);
+  // th dpendecies is empty just to mak it run once
+
+  if (!dogPic) return <p>Loading...</p>; // show loadig while react is still rendering the page
+
+  return <img src={dogPic} alt="A Random Dog" />;
+}
+
+export default App;
